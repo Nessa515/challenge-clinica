e9c4a415a5318532ded38df8e830281a1f1bdb51
Nessa515 <93387954+Nessa515@users.noreply.github.com>
2023-06-05 14:25:05 -0400

    Atualizado

--- src/tutors-pet.ts
+++ src/tutors-pet.ts
@@ -46,54 +46,6 @@ const tutor: typeTutor =
         ]   
 
     }
-    
-    // { 
-    //     id: 2, 
-    //     name: "Marcos Silva", 
-    //     phone: 854545323895, 
-    //     email: "marcos.silva@compasso.com", 
-    //     date_of_birth: "1998-10-12 10:10", 
-    //     zip_code: 12360000, 
-        
-    //     pets: 
-        
-    //     [ 
-    //         { 
-    //             id: 2, 
-    //             name: "Puf", 
-    //             species: "cat", 
-    //             carry: "p", 
-    //             weight: 5,
-    //             date_of_birth: "1990-08-24 09:11" 
-        
-    //         } 
-    //     ]   
-
-    // },
-
-    // { 
-    //     id: 3, 
-    //     name: "Mariana Paz", 
-    //     phone: 854000323895, 
-    //     email: "mariana.paz@compasso.com", 
-    //     date_of_birth: "1998-10-12 11:10", 
-    //     zip_code: 12363000, 
-        
-    //     pets: 
-        
-    //     [ 
-    //         { 
-    //             id: 3, 
-    //             name: "Lupi", 
-    //             species: "dog", 
-    //             carry: "p", 
-    //             weight: 5,
-    //             date_of_birth: "1996-01-15 18:10" 
-        
-    //         } 
-    //     ]   
-
-    // }
 ]
 
 export {tutor};
